5bf44c074e31cf1eac42365e5336f343f91e571c
fadiHB <eng.fadi.hb@gmail.com>
2020-10-16 22:32:28 +0300

    just push

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,31 +1,35 @@
 'use strict';
 
 // Cart constructor.
-var Cart = function(items) {
+var Cart = function (items) {
   // this.items is an array of CartItem instances.
   this.items = items;
 };
 
-Cart.prototype.addItem = function(product, quantity) {
+Cart.prototype.addItem = function (product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  this.items.push(new CartItem(product, quantity));
 };
 
-Cart.prototype.saveToLocalStorage = function() {
+Cart.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cart', JSON.stringify(this.items));
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function (item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  const index = this.items.indexOf(item);
+  this.items.splice(index, 1);
 };
 
-var CartItem = function(product, quantity) {
+var CartItem = function (product, quantity) {
   this.product = product;
   this.quantity = quantity;
 };
 
 // Product contructor.
-var Product = function(filePath, name) {
+var Product = function (filePath, name) {
   this.filePath = filePath;
   this.name = name;
   Product.allProducts.push(this);
